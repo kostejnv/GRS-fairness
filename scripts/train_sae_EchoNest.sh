e60f43639e025185b6f9e371c9eff5dc70af8a72
Vit Kostejn <v.kostejn.vk@gmail.com>
2025-05-05 20:29:01 +0200

    Update train sae scripts

--- scripts/train_sae_EchoNest.sh
+++ scripts/train_sae_EchoNest.sh
@@ -2,7 +2,7 @@
 
 base_run_id=ca2a47b49d4b4cc78ad2da62a6e02123
 
-python train_sae.py --dataset EchoNest --base_run_id "$base_run_id" --batch_size 1024 --epochs 10000 --early_stop 250 --seed 42 --embedding_dim 1024  --top_k 16 --reconstruction_loss L2 --auxiliary_coef 0.03125 --contrastive_coef 0
+python train_sae.py --dataset EchoNest --base_run_id "$base_run_id" --batch_size 1024 --epochs 10000 --early_stop 250 --seed 42 --embedding_dim 1024  --top_k 32 --reconstruction_loss L2 --auxiliary_coef 0.03125 --contrastive_coef 0
 
 python train_sae.py --dataset EchoNest --base_run_id "$base_run_id" --batch_size 1024 --epochs 10000 --early_stop 250 --seed 42 --embedding_dim 1024  --top_k 64 --reconstruction_loss L2 --auxiliary_coef 0.03125 --contrastive_coef 0
 
--- scripts/train_sae_LastFm1k.sh
+++ scripts/train_sae_LastFm1k.sh
@@ -2,7 +2,7 @@
 
 base_run_id=4a43996d7eec489183ad0d6b0c00d935
 
-python train_sae.py --dataset LastFM1k --base_run_id "$base_run_id" --batch_size 1024 --epochs 10000 --early_stop 250 --seed 42 --embedding_dim 1024  --top_k 16 --reconstruction_loss L2 --auxiliary_coef 0.03125 --contrastive_coef 0
+python train_sae.py --dataset LastFM1k --base_run_id "$base_run_id" --batch_size 1024 --epochs 10000 --early_stop 250 --seed 42 --embedding_dim 1024  --top_k 32 --reconstruction_loss L2 --auxiliary_coef 0.03125 --contrastive_coef 0
 
 python train_sae.py --dataset LastFM1k --base_run_id "$base_run_id" --batch_size 1024 --epochs 10000 --early_stop 250 --seed 42 --embedding_dim 1024  --top_k 64 --reconstruction_loss L2 --auxiliary_coef 0.03125 --contrastive_coef 0
 
