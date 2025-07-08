a1837f3e660172e1e98f285f5e92d731862eaaec
Vit Kostejn <v.kostejn.vk@gmail.com>
2025-07-08 00:56:50 -0700

    Delete uncessary parts

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,11 +1,9 @@
 from .dataset_loader import DatasetLoader
-from .echoNest_loader import EchoNestLoader
 from .lastFm1k_loader import LastFm1kLoader
 from .movieLens_loader import MovieLensLoader
 from .data_loader import DataLoader
 
 __all__ = [
-    'EchoNestLoader',
     'DatasetLoader',
     'LastFm1kLoader',
     'MovieLensLoader',
--- scripts/download_echoNest_dataset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p data
-wget -O data/EchoNest.zip http://labrosa.ee.columbia.edu/~dpwe/tmp/train_triplets.txt.zip
-unzip -o data/EchoNest.zip -d data
-rm -f data/EchoNest.zip
-mv data/train_triplets.txt data/EchoNest.txt
